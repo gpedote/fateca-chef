3077f0b1681a8063a0f33a3f21e81253d7f1b22a
Gabriel Pedote <gabriel.pedote@fatec.sp.gov.br>
2014-03-30 18:31:07 -0300

    Default attributes for Fateca cookbook

---
+++ cookbooks/fateca/attributes/default.rb
@@ -0,0 +1,7 @@
+default['fateca']['path'] = '/var/www/apps/fateca/'
+default['fateca']['repo'] = 'https://github.com/gpedote/fateca.git'
+default['fateca']['revision'] = 'master'
+default['fateca']['owner'] = 'vagrant'
+default['fateca']['group'] = 'vagrant'
+default['fateca']['submodules'] = false
+default['fateca']['force_deploy'] = false
